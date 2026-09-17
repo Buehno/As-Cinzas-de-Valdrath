c1b6f87ef23d2b3167adb1855dccaadef8c04270
Buehno <ronaldo.bueno@iagentics.com>
2026-09-17 12:02:50 -0300

    first commit

---
+++ Valdrath/src/Historia.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Historia {
+    public static void Introducao(Scanner scanner) {
+        System.out.println("╔══════════════════════════════════════════╗\r\n" + //
+                        "║                                          ║\r\n" + //
+                        "║   No princípio, houve a Chama.           ║\r\n" + //
+                        "║                                          ║\r\n" + //
+                        "╚══════════════════════════════════════════╝");
+        SpriteRender.desenhar("sprites/voz_da_chama.png");
+          scanner.nextLine();
+        System.out.println(" .~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~.\r\n" + //
+                        " :                                            :\r\n" + //
+                        " :   Dizem que ela caiu dos céus há mil anos, :\r\n" + //
+                        " :   e que mantém Valdrath viva.              :\r\n" + //
+                        " :                                            :\r\n" + //
+                        " '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'");
+                          scanner.nextLine();
+        System.out.println(" .~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~.\r\n" + //
+                        " :                                            :\r\n" + //
+                        " :   Mas a luz começou a falhar.              :\r\n" + //
+                        " :   Uma praga rasteja pela terra.            :\r\n" + //
+                        " :   A Ordem tem um culpado: Malachar.        :\r\n" + //
+                        " :                                            :\r\n" + //
+                        " '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'");
+        scanner.nextLine();
+        System.out.println(" .~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~.\r\n" + //
+                        " :                                            :\r\n" + //
+                        " :   Mas a luz começou a falhar.              :\r\n" + //
+                        " :   Uma praga rasteja pela terra.            :\r\n" + //
+                        " :   A Ordem tem um culpado: Malachar.        :\r\n" + //
+                        " :                                            :\r\n" + //
+                        " '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'");
+        System.out.println(" .~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~.\r\n" + //
+                        " :                                            :\r\n" + //
+                        " :   E tem uma solução: você.                 :\r\n" + //
+                        " :                                            :\r\n" + //
+                        " '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'");
+        scanner.nextLine();
+        
+    }
+}
